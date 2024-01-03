698e9403ac0e46f0e96caf3562aff080330c7479
maxjdev <maxjramosdev@gmail.com>
2024-01-03 14:36:58 -0300

    add(provider): configuring provider in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import SideBar from './containers/SideBar'
 import TaskList from './containers/TaskList'
 import GlobalStyle, { Container } from './styles'
+import { Provider } from 'react-redux'
+import store from './store'
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyle />
       <Container>
         <SideBar />
         <TaskList />
       </Container>
-    </>
+    </Provider>
   )
 }
 
