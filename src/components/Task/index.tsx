39584e61c69be9d3bc62b262ade1d55b7883da7d
maxjdev <maxjramosdev@gmail.com>
2024-01-03 11:37:03 -0300

    added: props and state in buttons

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,16 +1,37 @@
+import { useState } from 'react'
 import * as S from './styles'
 
-const Task = () => (
-  <S.Card>
-    <S.Title>Name task</S.Title>
-    <S.Tag>important</S.Tag>
-    <S.Tag>pendencies</S.Tag>
-    <S.Description />
-    <S.ActionBar>
-      <S.Button>Edit</S.Button>
-      <S.Button>Remove</S.Button>
-    </S.ActionBar>
-  </S.Card>
-)
+type Props = {
+  title: string
+  priority: string
+  status: string
+  description: string
+}
+
+const Task = ({ title, priority, status, description }: Props) => {
+  const [isEditing, setIsEditing] = useState(false)
+
+  return (
+    <S.Card>
+      <S.Title>{title}</S.Title>
+      <S.Tag>{priority}</S.Tag>
+      <S.Tag>{status}</S.Tag>
+      <S.Description value={description} />
+      <S.ActionBar>
+        {isEditing ? (
+          <>
+            <S.Button>Save</S.Button>
+            <S.Button onClick={() => setIsEditing(false)}>Cancel</S.Button>
+          </>
+        ) : (
+          <>
+            <S.Button onClick={() => setIsEditing(true)}>Edit</S.Button>
+            <S.Button>Remove</S.Button>
+          </>
+        )}
+      </S.ActionBar>
+    </S.Card>
+  )
+}
 
 export default Task
--- src/containers/TaskList/index.tsx
+++ src/containers/TaskList/index.tsx
@@ -1,25 +1,41 @@
 import Task from '../../components/Task'
 import { Container } from './styles'
 
+const tasks = [
+  {
+    title: 'Study Typescript',
+    description: 'Watch the class',
+    priority: 'Important',
+    status: 'Done'
+  },
+  {
+    title: 'Study Angular',
+    description: 'Watch the class',
+    priority: 'Very important',
+    status: 'Pending'
+  },
+  {
+    title: 'Study Java',
+    description: 'Watch the class',
+    priority: 'Urgent',
+    status: 'Pending'
+  }
+]
+
 const TaskList = () => (
   <Container>
     <p>2 tasks marked as: &quot;category&quot; and &quot;term&quot;</p>
     <ul>
-      <li>
-        <Task />
-      </li>
-      <li>
-        <Task />
-      </li>
-      <li>
-        <Task />
-      </li>
-      <li>
-        <Task />
-      </li>
-      <li>
-        <Task />
-      </li>
+      {tasks.map((t) => (
+        <li key={t.title}>
+          <Task
+            description={t.description}
+            priority={t.priority}
+            status={t.status}
+            title={t.title}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
