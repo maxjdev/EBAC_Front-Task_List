35335dcec72e325c577e1d3452854751efe8f91d
maxjdev <maxjramosdev@gmail.com>
2024-01-03 14:37:45 -0300

    working states

--- src/containers/TaskList/index.tsx
+++ src/containers/TaskList/index.tsx
@@ -1,44 +1,28 @@
+import { useSelector } from 'react-redux'
 import Task from '../../components/Task'
 import { Container } from './styles'
-import * as enums from '../../utils/enums/Task'
+import { RootReducer } from '../../store'
 
-const tasks = [
-  {
-    title: 'Study Typescript',
-    description: 'Watch the class',
-    priority: enums.Priority.NORMAL,
-    status: enums.Status.DONE
-  },
-  {
-    title: 'Study Angular',
-    description: 'Watch the class',
-    priority: enums.Priority.IMPORTANT,
-    status: enums.Status.PENDENT
-  },
-  {
-    title: 'Study Java',
-    description: 'Watch the class',
-    priority: enums.Priority.URGENT,
-    status: enums.Status.PENDENT
-  }
-]
+const TaskList = () => {
+  const { tasks } = useSelector((state: RootReducer) => state)
 
-const TaskList = () => (
-  <Container>
-    <p>2 tasks marked as: &quot;category&quot; and &quot;term&quot;</p>
-    <ul>
-      {tasks.map((t) => (
-        <li key={t.title}>
-          <Task
-            description={t.description}
-            priority={t.priority}
-            status={t.status}
-            title={t.title}
-          />
-        </li>
-      ))}
-    </ul>
-  </Container>
-)
+  return (
+    <Container>
+      <p>2 tasks marked as: &quot;category&quot; and &quot;term&quot;</p>
+      <ul>
+        {tasks.map((t) => (
+          <li key={t.title}>
+            <Task
+              description={t.description}
+              priority={t.priority}
+              status={t.status}
+              title={t.title}
+            />
+          </li>
+        ))}
+      </ul>
+    </Container>
+  )
+}
 
 export default TaskList
